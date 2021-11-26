1e8e8ac9327a503aad67b6d3fecfbdec6f30c6ff
Aleksandr Balaban <balabanaleksandr45@gmail.com>
2021-11-26 11:27:07 +0300

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
-import Layout from "../components/layouts/main/Layout";
-import url from "url";
-import {initializeStore} from "../store/store";
-import {fetchFeedbackData} from "../store/catalog/actions";
 
 export default function Home() {
   return (
